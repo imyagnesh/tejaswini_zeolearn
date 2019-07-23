b5a35d89d7cfc1078e3b26bc44d369585bb38bf7
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2019-07-23 23:47:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var container = document.getElementById("main");
+
+container.addEventListener("click", function() {
+  container.style.backgroundColor = "green";
+});
+
+console.log(container);
+var node = document.createElement("h1");
+container.appendChild(node);
+// container.appendChild(<h1>Hello</h1>);
