6de28fa73a0bf1cf00539f7708ba960e27aabd6d
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2019-07-30 00:12:11 +0530

    Commit code

--- app/controller/Users.js
+++ app/controller/Users.js
@@ -14,7 +14,7 @@ Ext.define("AM.controller.Users", {
       userlist: {
         itemdblclick: this.editUser
       },
-      "useredit buttons[action=save]": {
+      "useredit button[action=save]": {
         click: this.updateUser
       }
     });
@@ -23,9 +23,23 @@ Ext.define("AM.controller.Users", {
   editUser: function(grid, record) {
     var view = Ext.widget("useredit");
     view.down("form").loadRecord(record);
-  }
+  },
 
   updateUser: function(button) {
-    console.log(clicked save button)
+    var win = button.up("window");
+    var form = win.down("form");
+    var record = form.getRecord();
+    var values = form.getValues();
+    console.log("record", record);
+    console.log("values", values);
+
+    record.set(values);
+    win.close();
+
+    this.getUsersStore().sync();
+
+    // var view = Ext.widget("useredit");
+    // var record2 = view.down("form").getRecord();
+    // console.log("record2", record2);
   }
 });
--- app/store/Users.js
+++ app/store/Users.js
@@ -1,14 +1,33 @@
 Ext.define("AM.store.Users", {
   extend: "Ext.data.Store",
   model: "AM.model.User",
-  data: [
-    {
-      name: "Yagnesh",
-      email: "yagnesh.modh@gmail.com"
+  autoLoad: true,
+  proxy: {
+    type: "ajax",
+    actionMethods: {
+      create: "POST",
+      read: "GET",
+      update: "PUT",
+      destroy: "POST"
     },
-    {
-      name: "Namrata",
-      email: "Namrata.modh@gmail.com"
+    api: {
+      read: "http://localhost:3004/users",
+      update: "http://localhost:3004/users"
+    },
+    reader: {
+      type: "json",
+      root: "users",
+      successProperty: "success"
     }
-  ]
+  }
+  // data: [
+  //   {
+  //     name: "Yagnesh",
+  //     email: "yagnesh.modh@gmail.com"
+  //   },
+  //   {
+  //     name: "Namrata",
+  //     email: "Namrata.modh@gmail.com"
+  //   }
+  // ]
 });
