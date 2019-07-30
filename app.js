e8855bd11d2c75b2db21b61add9fc911aa562f82
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2019-07-30 23:58:56 +0530

    Commited code for component & container

--- app.js
+++ app.js
@@ -1,15 +1,87 @@
+var myComponent = Ext.create("Ext.Component", {
+  html: "My First ExtJS Application",
+  x: 50,
+  y: 50
+});
+
+var myComponent1 = Ext.create("Ext.Component", {
+  html: "My Second ExtJS Application"
+});
+
+var myContainer = Ext.create("Ext.container.Container", {
+  items: [myComponent, myComponent1],
+  layout: "absolute"
+});
+
 Ext.application({
   requires: ["Ext.container.Viewport"],
-  name: "AM",
-  controllers: ["Users"],
-  appFolder: "app",
+  // name: "AM",
+  // controllers: ["Users"],
+  // appFolder: "app",
   launch: function() {
     Ext.create("Ext.container.Viewport", {
-      layout: "fit",
       items: [
-        {
-          xtype: "userlist"
-        }
+        myContainer
+        // {
+        //   xtype: "button",
+        //   text: "Disable",
+        //   handler: function() {
+        //     this.up()
+        //       .down("#myComponent")
+        //       .disable();
+        //   }
+        // },
+        // {
+        //   xtype: "button",
+        //   text: "Enable",
+        //   handler: function() {
+        //     this.up()
+        //       .down("#myComponent")
+        //       .enable();
+        //   }
+        // },
+        // {
+        //   xtype: "button",
+        //   text: "Show",
+        //   handler: function() {
+        //     this.up()
+        //       .down("#myComponent")
+        //       .show();
+        //   }
+        // },
+        // {
+        //   xtype: "button",
+        //   text: "Hide",
+        //   handler: function() {
+        //     this.up()
+        //       .down("#myComponent")
+        //       .hide();
+        //   }
+        // },
+        // {
+        //   // xtype: "component",
+        //   // html: "my First ExtJs Application",
+        //   itemId: "myComponent",
+        //   tpl: "name: {firstName} {lastName}",
+        //   data: {
+        //     firstName: "Yagnesh",
+        //     lastName: "Modh"
+        //   },
+        //   padding: 20,
+        //   width: 200,
+        //   height: 200,
+        //   style: {
+        //     border: "2px solid red"
+        //   },
+        //   resizable: true,
+        //   listeners: {
+        //     resize: {
+        //       fn: function(component, width, height) {
+        //         console.log("w/h", width, height);
+        //       }
+        //     }
+        //   }
+        // }
       ]
     });
   }
